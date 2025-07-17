3911b5bc05a55c971ededaff24c377d8af4086b9
maxcampanella <152271923+maxcampanella@users.noreply.github.com>
2025-07-17 17:00:44 +0200

    Update torneo.py

--- torneo.py
+++ torneo.py
@@ -11,7 +11,7 @@ squadre = ["MAX", "SERGIO", "LEO", "GIANNI", "PASSE", "COMPA", "TONY", "CHRI"]
 partite = list(itertools.combinations(squadre, 2))
 
 # Dizionario classifica
-classifica = {s: {"PUNTI": 0, "Vittorie": 0, "Pareggi": 0, "Sconfitte": 0} for s in squadre}
+classifica = {s: {"Punti": 0, "Vittorie": 0, "Pareggi": 0, "Sconfitte": 0} for s in squadre}
 
 # Mostra ogni partita e chiedi il risultato
 st.subheader("Viva la democrazia")
@@ -37,16 +37,16 @@ for i, (s1, s2) in enumerate(partite):
 
     # Solo se è stata fatta una scelta valida
     if risultato == s1:
-        classifica[s1]["PUNTI"] += 3
+        classifica[s1]["Punti"] += 3
         classifica[s1]["Vittorie"] += 1
         classifica[s2]["Sconfitte"] += 1
     elif risultato == s2:
-        classifica[s2]["PUNTI"] += 3
+        classifica[s2]["Punti"] += 3
         classifica[s2]["Vittorie"] += 1
         classifica[s1]["Sconfitte"] += 1
     elif risultato == "pareggio":
-        classifica[s1]["PUNTI"] += 1
-        classifica[s2]["PUNTI"] += 1
+        classifica[s1]["Punti"] += 1
+        classifica[s2]["Punti"] += 1
         classifica[s1]["Pareggi"] += 1
         classifica[s2]["Pareggi"] += 1
 
@@ -55,7 +55,7 @@ for i, (s1, s2) in enumerate(partite):
 
 # Costruzione classifica
 df = pd.DataFrame.from_dict(classifica, orient="index")
-df = df.sort_values(by=["PUNTI", "Vittorie"], ascending=False)
+df = df.sort_values(by=["Punti", "Vittorie"], ascending=False)
 
 # Spazio visivo prima della classifica
 for _ in range(20):
