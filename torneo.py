b52085933d0e888a547d5bd2b4784169bef1752d
maxcampanella <152271923+maxcampanella@users.noreply.github.com>
2025-08-16 20:38:13 +0200

    Update torneo.py

--- torneo.py
+++ torneo.py
@@ -19,7 +19,7 @@ squadre = [
     "OLMO", "PUTRINO", "MINNITI", "CRUSH_CULO", "BELTRAME", "INVERNIZZI",
     "DIVITTORIO", "FANT", "PERO", "DELSIGNORE", "RANA", "BEMBO", 
     "IMPALONI", "PEZZANA", "GIANCOLA"
-]
+],
 __________________________________________________________________________________________
 
 # Mischia le partite solo una volta
@@ -77,4 +77,5 @@ for _ in range(20):
 
 st.subheader("🏆 Classifica finale")
 st.dataframe(df.style.format(precision=0))
+
 
