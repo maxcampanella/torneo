fc6a092f88954784380403a348cce0aa13fc52e6
maxcampanella <152271923+maxcampanella@users.noreply.github.com>
2025-08-18 10:04:50 +0200

    Update torneo.py

--- torneo.py
+++ torneo.py
@@ -7,18 +7,11 @@ st.title("LA BRISCOLA SICILIANA")
 
 #__________________________________SQUADRE_________________________________________________
 
-#squadre = ["MAX", "SERGIO", "LEO", "GIANNI", "PASSE", "COMPA", "TONY", "CHRI"]
+squadre = ["MAX", "SERGIO", "LEO", "GIANNI", "PASSE", "COMPA", "TONY", "CHRI"]
 
 
-squadre = [
-    "ZOCCHI", "BARCHI", "MATTIUZ", "ABRUSCATO", "DESSILANI",
-    "CHIESA", "MUCCINO", "MANCUSO", 
-    "BALZANO", "IACOMETTI", "SARONNE", "CESTI",
-    "COMOLLA", "TAGLIABUE", "SANTEUSANIO", "PEZZOTTA", "PANELLA", "MARI", "CODINI",
-    "REPOSSI", "MAGGIORA", "MAGGIORI", "FACHERIS", "PERONI", "TOMOVA",
-    "OLMO", "PUTRINO", "MINNITI", "CRUSH_CULO", "BELTRAME", "INVERNIZZI",
-    "DIVITTORIO", "FANT", "PERO", "DELSIGNORE", "RANA", "BEMBO", 
-    "IMPALONI", "PEZZANA", "GIANCOLA" ]
+#squadre = [
+#    "ZOCCHI", "BARCHI", "MATTIUZ", "ABRUSCATO", "DESSILANI", "CHIESA", "MUCCINO", "MANCUSO", "BALZANO", "IACOMETTI", "SARONNE", "CESTI", "COMOLLA", "TAGLIABUE", "SANTEUSANIO", "PEZZOTTA", "PANELLA", "MARI", "CODINI", "REPOSSI", "MAGGIORA", "MAGGIORI", "FACHERIS", "PERONI", "TOMOVA", "OLMO", "PUTRINO", "MINNITI", "CRUSH_CULO", "BELTRAME", "INVERNIZZI", "DIVITTORIO", "FANT", "PERO", "DELSIGNORE", "RANA", "BEMBO", "IMPALONI", "PEZZANA", "GIANCOLA" ]
 #__________________________________________________________________________________________
 
 # Mischia le partite solo una volta
@@ -76,6 +69,7 @@ for _ in range(20):
 
 st.subheader("🏆 Classifica finale")
 st.dataframe(df.style.format(precision=0))
+
 
 
 
